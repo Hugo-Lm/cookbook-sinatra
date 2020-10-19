c48f950b0d557a3cec3d9dca5f44bf1057a9b51d
Hugo Le Morvan <lm.hugo@hotmail.fr>
2020-10-19 18:11:31 +0200

    Exercice Sinatra à continuer

--- app.rb
+++ app.rb
@@ -8,6 +8,22 @@ configure :development do
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
-get '/' do
-  'Hello world! Hugo speaking'
+get '/' do   # <-- Router
+  @usernames = [ 'ssaunier', 'Papillard' ]
+  erb :index  # <-- Controller
 end
+
+get '/about' do
+  erb :about
+end
+
+get '/team/:username' do
+  binding.pry
+  puts params[:username]
+  "The username is #{params[:username]}"
+end
+
+get '/layout' do
+  erb :layout
+end
+# Launch Router & Controller : bundle exec ruby app.rb
